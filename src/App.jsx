e3328deed72cc8268e0e7fb8d6da64738c1b9a48
Calvin Hinze <calvinhinzer@gmail.com>
2024-10-16 23:08:59 +0200

    [TASK] improved flow axis

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,99 @@
 import { useRef, useEffect } from "react"
 import { useAtom } from "jotai"
 
+import "@/styles/Slide.css"
 import { currentSlideAtom, slidesAtom } from "@/store/atoms"
 
 import useGamepad from "@/hooks/useGamepad"
 import useKeyDown from "@/hooks/useKeydown"
 import useFullscreen from "@/hooks/useFullScreen"
 
-import { Main, Article, Section } from "@/components/styled/Slideshow"
-import { Progress } from "@/components/styled/Progess"
+import { Slideshow } from "@/components/styled/Slideshow"
+import { Progress } from "@/components/styled/Progress"
 
 export function App() {
-  const { buttons, axes } = useGamepad(true)
+  const [slidesData] = useAtom(slidesAtom)
+  const MAX_VALUE = slidesData[0].length - 1
   const [isFullscreen, toggleFullscreen] = useFullscreen()
 
-  const [currentSlide, setCurrentSlide] = useAtom(currentSlideAtom)
+  const { buttons, axes } = useGamepad(true)
 
-  const [slidesData] = useAtom(slidesAtom)
+  // const [currentSlide, setCurrentSlide] = useAtom(currentSlideAtom)
 
-  const MAX_VALUE = slidesData[0].length - 1
+  // useEffect(() => {
+  //   setCurrentSlide((prev) => ({
+  //     ...prev,
+  //     y: new Array(slidesData[0].length).fill(0),
+  //   }))
+  // }, [])
 
-  useEffect(() => {
-    setCurrentSlide((prev) => ({
-      ...prev,
-      y: new Array(slidesData[0].length).fill(0),
-    }))
-  }, [])
+  // const updateXValue = (newX) => {
+  //   if (newX >= 0 && newX <= MAX_VALUE) {
+  //     setCurrentSlide((prev) => ({
+  //       ...prev,
+  //       x: newX,
+  //     }))
+  //   }
+  // }
 
-  const updateXValue = (newX) => {
-    if (newX >= 0 && newX <= MAX_VALUE) {
-      setCurrentSlide((prev) => ({
-        ...prev,
-        x: newX,
-      }))
-    }
-  }
+  // const updateYValue = (newY) => {
+  //   setCurrentSlide((prev) => {
+  //     const newYArray = [...prev.y]
 
-  const updateYValue = (newY) => {
-    setCurrentSlide((prev) => {
-      const newYArray = [...prev.y]
+  //     if (prev.x >= 0 && prev.x < newYArray.length) {
+  //       newYArray[prev.x] = newY < 0 ? 0 : newY // Ensure y value is never negative
+  //     }
+  //     return {
+  //       ...prev,
+  //       y: newYArray,
+  //     }
+  //   })
+  // }
+  // useKeyDown((event) => {
+  //   console.log(currentSlide)
+  //   console.log("X: " + currentSlide.x)
+  //   console.log("Y: " + currentSlide.y)
 
-      if (prev.x >= 0 && prev.x < newYArray.length) {
-        newYArray[prev.x] = newY < 0 ? 0 : newY // Ensure y value is never negative
-      }
-      return {
-        ...prev,
-        y: newYArray,
-      }
-    })
-  }
-  useKeyDown((event) => {
-    console.log(currentSlide)
-    console.log("X: " + currentSlide.x)
-    console.log("Y: " + currentSlide.y)
+  //   switch (event.key) {
+  //     case "ArrowRight":
+  //       updateXValue(currentSlide.x + 1)
+  //       break
+  //     case "ArrowLeft":
+  //       updateXValue(currentSlide.x - 1)
+  //       break
 
-    switch (event.key) {
-      case "ArrowRight":
-        updateXValue(currentSlide.x + 1)
-        break
-      case "ArrowLeft":
-        updateXValue(currentSlide.x - 1)
-        break
+  //     case "ArrowDown":
+  //       updateYValue(currentSlide.y[currentSlide.x] + 1)
 
-      case "ArrowDown":
-        updateYValue(currentSlide.y[currentSlide.x] + 1)
-
-        break
-      case "ArrowUp":
-        updateYValue(currentSlide.y[currentSlide.x] - 1)
-        break
-      case "f":
-        toggleFullscreen()
-        break
-      case "F":
-        toggleFullscreen()
-        break
-    }
-  })
+  //       break
+  //     case "ArrowUp":
+  //       updateYValue(currentSlide.y[currentSlide.x] - 1)
+  //       break
+  //     case "f":
+  //       toggleFullscreen()
+  //       break
+  //     case "F":
+  //       toggleFullscreen()
+  //       break
+  //   }
+  // })
 
   // Gamepad API
-  useEffect(() => {
-    if (buttons[15] || buttons[1]) updateXValue(currentSlide.x + 1)
-    if (buttons[14] || buttons[2]) updateXValue(currentSlide.x - 1)
+  // useEffect(() => {
+  //   if (buttons[15] || buttons[1]) updateXValue(currentSlide.x + 1)
+  //   if (buttons[14] || buttons[2]) updateXValue(currentSlide.x - 1)
 
-    if (buttons[13] || buttons[0])
-      updateYValue(currentSlide.y[currentSlide.x] + 1)
-    if (buttons[12] || buttons[3])
-      updateYValue(currentSlide.y[currentSlide.x] + 1)
+  //   if (buttons[13] || buttons[0])
+  //     updateYValue(currentSlide.y[currentSlide.x] + 1)
+  //   if (buttons[12] || buttons[3])
+  //     updateYValue(currentSlide.y[currentSlide.x] + 1)
 
-    if (buttons[6] || buttons[7]) toggleFullscreen()
-  }, [buttons, axes])
+  //   if (buttons[6] || buttons[7]) toggleFullscreen()
+  // }, [buttons, axes])
 
   return (
     <>
-      <Main $currentSlideX={currentSlide.x}>
+      {/* <Main $currentSlideX={currentSlide.x}>
         {slidesData &&
           slidesData[0].map((article, index) => {
             return (
@@ -116,8 +116,9 @@ export function App() {
               </Article>
             )
           })}
-      </Main>
-      <Progress max={MAX_VALUE} value={currentSlide.x} />
+      </Main> */}
+      <Slideshow data={slidesData[0]} max={MAX_VALUE} />
+      <Progress max={MAX_VALUE} value={0} />
     </>
   )
 }
--- src/components/styled/Slideshow.jsx
+++ src/components/styled/Slideshow.jsx
@@ -1,39 +1,119 @@
-import { styled } from "styled-components"
+import { useEffect, useState } from "react"
+import { atom, useAtom } from "jotai"
+import styled from "@emotion/styled"
 import { COLOR_BACKGROUND, COLOR_BACKGROUND_DARK } from "@/store/base"
+import useGamepad from "@/hooks/useGamepad"
+import useKeyDown from "@/hooks/useKeydown"
 import "@/styles/Slide.css"
-import { useState } from "react"
 
 // Vertical Srcoll
-export const Main = styled.main`
+const Main = styled.main`
+  outline: 1px solid blue;
   height: 100%;
   width: 100%;
   display: flex;
   flex-direction: row;
   transition: 0.5s transform linear;
   will-change: transform;
-  transform: translateX(${(props) => props.$currentSlideX * -100}%);
+  transform: translateX(${(props) => props.currentArticle * -100}%);
 `
 
+export const Slideshow = ({ data, max }) => {
+  const [currentArticle, setCurrentArticle] = useState(0)
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowRight":
+        if (currentArticle < max) {
+          setCurrentArticle(currentArticle + 1)
+        }
+        break
+      case "ArrowLeft":
+        if (currentArticle > 0) {
+          setCurrentArticle(currentArticle - 1)
+        }
+        break
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [handleKeyDown])
+
+  return (
+    <Main currentArticle={currentArticle}>
+      {data.map((article, index) => (
+        <ArticleWrapper
+          key={index}
+          max={article.length - 1}
+          index={index}
+          currentArticle={currentArticle}
+        >
+          {article.map((section, index) => {
+            const el = section[0]
+            return <SectionWrapper key={index}>{<el.default />}</SectionWrapper>
+          })}
+        </ArticleWrapper>
+      ))}
+    </Main>
+  )
+}
+
 // Horizontal Srcoll
-export const Article = styled.article`
+const Article = styled.article`
+  outline: 1px solid green;
   width: 100%;
   height: 100%;
   flex: 0 0 auto;
-  &.subslide {
-    display: flex;
-    flex-direction: column;
-    transition: 0.5s transform linear;
-    will-change: transform;
-    /* only trigger animation when scroll down not sideways */
-    transform: translateY(${(props) => props.$currentSlideY * -100}%);
-  }
+
+  display: flex;
+  flex-direction: column;
+  transition: 0.5s transform linear;
+  will-change: transform;
+  transform: translateY(${(props) => props.currentSection * -100}%);
+
   background-color: ${COLOR_BACKGROUND};
   &:first-of-type {
     background-color: ${COLOR_BACKGROUND_DARK};
   }
 `
 
+const ArticleWrapper = (props) => {
+  const [currentSection, setCurrentSection] = useState(0)
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowDown":
+        if (currentSection < props.max) {
+          setCurrentSection(currentSection + 1)
+        }
+        break
+      case "ArrowUp":
+        if (currentSection > 0) {
+          setCurrentSection(currentSection - 1)
+        }
+        break
+    }
+  }
+
+  useEffect(() => {
+    if (props.currentArticle === props.index)
+      window.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [handleKeyDown])
+
+  return <Article currentSection={currentSection}>{props.children}</Article>
+}
+
 export const Section = styled.section`
+  outline: 1px solid red;
   width: 100%;
   height: 100%;
   display: grid;
@@ -42,9 +122,6 @@ export const Section = styled.section`
   gap: 1rem;
 `
 
-// Article
-const Slideshow = (props) => {
-  const [currentPosition, setCurrentPosition] = useState(0)
-
-  return <Article>{props.children}</Article>
+const SectionWrapper = (props) => {
+  return <Section>{props.children}</Section>
 }
